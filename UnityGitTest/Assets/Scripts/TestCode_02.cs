1ca2cb466e4dcffd2cf360f3e10f4855a5017a96
seawind11 <seawind11g@gmail.com>
2017-05-17 14:00:21 +0900

    클래스 추가.

---
+++ UnityGitTest/Assets/Scripts/TestCode_02.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode_02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    /// <summary>
+    /// TEST
+    /// </summary>
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
